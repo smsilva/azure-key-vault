754617ccf2c6b9a7b3df119d97cffb7897f93b32
Silvio Silva <smsilva@gmail.com>
2021-09-15 19:55:45 -0300

    WIP

--- src/main.tf
+++ src/main.tf
@@ -7,9 +7,8 @@ locals {
 
 resource "random_string" "key_vault_id" {
   keepers = {
-    name                   = var.name
-    platform_instance_name = var.platform_instance_name
-    location               = var.resource_group.location
+    name     = var.name
+    location = var.resource_group.location
   }
 
   length      = 3
@@ -27,61 +26,4 @@ resource "azurerm_key_vault" "default" {
   sku_name                   = var.sku_name
   soft_delete_retention_days = var.soft_delete_retention_days
   purge_protection_enabled   = false # Should we change this to not allow a key vault be purged before retention period
-}
-
-resource "azurerm_key_vault_access_policy" "administrator" {
-  for_each = toset(local.administrator_objects_list)
-
-  key_vault_id = azurerm_key_vault.default.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = each.value
-
-  certificate_permissions = [
-    "Backup",
-    "Create",
-    "Delete",
-    "DeleteIssuers",
-    "Get",
-    "GetIssuers",
-    "Import",
-    "List",
-    "ListIssuers",
-    "ManageContacts",
-    "ManageIssuers",
-    "Purge",
-    "Recover",
-    "Restore",
-    "SetIssuers",
-    "Update",
-  ]
-
-  key_permissions = [
-    "Backup",
-    "Create",
-    "Decrypt",
-    "Delete",
-    "Encrypt",
-    "Get",
-    "Import",
-    "List",
-    "Purge",
-    "Recover",
-    "Restore",
-    "Sign",
-    "UnwrapKey",
-    "Update",
-    "Verify",
-    "WrapKey",
-  ]
-
-  secret_permissions = [
-    "Backup",
-    "Delete",
-    "Get",
-    "List",
-    "Purge",
-    "Recover",
-    "Restore",
-    "Set",
-  ]
 }
---
+++ src/security.tf
@@ -0,0 +1,56 @@
+resource "azurerm_key_vault_access_policy" "administrator" {
+  for_each = toset(local.administrator_objects_list)
+
+  key_vault_id = azurerm_key_vault.default.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = each.value
+
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "Recover",
+    "Restore",
+    "SetIssuers",
+    "Update",
+  ]
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+  ]
+}
